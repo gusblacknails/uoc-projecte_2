eec71f030e81456845348cd91718c740b368bbc5
Gus Escola <camagrock@gmail.com>
2025-02-27 16:50:45 +0100

    first program from pac_1

---
+++ sketch.js
@@ -0,0 +1,64 @@
+let img;
+
+let tamanyOriginal = true;
+function preload() {
+  img = loadImage("imatge_projecte_2.webp");
+}
+function setup() {
+  createCanvas(1000, 1000);
+  background(50);
+  image(img, 2, 2);
+}
+function dilatacioImatge() {
+  if (tamanyOriginal) {
+    console.log("1");
+    image(img, 0, 0, 1500, 1500);
+    tamanyOriginal = false;
+  }
+  else {
+    console.log("2");
+    image(img, 2, 2);
+    
+    tamanyOriginal = true;
+  }  
+}
+function posteritzacio() {
+  img.filter(POSTERIZE, 3);
+  image(img, 0, 0);
+}
+function binaritzacio() {
+  img.filter(THRESHOLD);
+  image(img, 0, 0);
+}
+function negatiu() {
+  img.filter(INVERT);
+  image(img, 0, 0);
+}
+function resetejar() {
+  tamanyOriginal = true;
+  img = loadImage("imatge_projecte_2.webp", () => {
+    createCanvas(1000, 1000);
+    background(50);
+    image(img, 2, 2);
+  });
+  
+}
+function keyPressed() {
+  if (key === "E" || key === "e") {
+    dilatacioImatge();
+  }
+  if (key === "S" || key === "s") {
+    posteritzacio();
+  }
+  if (key === "C" || key === "c") {
+    binaritzacio();
+  }
+  if (key === "O" || key === "o") {
+    negatiu();
+  }
+
+  if (key != "E" && key != "e" && key != "S" && key != "s" && key != "C" && key != "c" && key != "O" && key != "o") {
+    resetejar();
+  }
+
+}
